117f00945090fe9865424ba1b98422f41bdc7b05
arenaguiose <37896208+arenaguiose@users.noreply.github.com>
2022-01-05 15:53:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ codes_naf = pd.read_csv("CODES_NAF_PERIMETRE.csv", encoding = "ISO-8859-1", sep=
 factu = pd.read_excel("Données facturation ptf banques.xlsx")
 export = pd.read_excel("Export 05_08_2021 16_41 (1).xlsx", sheet_name="Résultats", nrows=20000)
 formes = pd.read_csv("FORMES_JURIDIQUES.csv", encoding = "ISO-8859-1", sep=";")
-SUL = pd.read_csv("StockUniteLegale_utf8.csv", low_memory=False, nrows=20000)
+sul = pd.read_csv("StockUniteLegale_utf8.csv", low_memory=False, nrows=20000)
 
 
-print(SUL.columns)
+print(sul.columns)
